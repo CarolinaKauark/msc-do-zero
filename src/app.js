79d8f6c7baae95a4489f332f46f51db2282b2f66
Ckauark <ckauark@gmail.com>
2022-11-10 17:50:43 -0300

    feat: new branch from the beginning

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const personRouter = require('./routes/person.routes');
 const app = express();
 
 app.use(express.json());
-
-app.use('/person', personRouter);
 
 module.exports = app;
