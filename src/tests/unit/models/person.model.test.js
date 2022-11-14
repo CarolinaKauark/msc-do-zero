8d1d1d9c8a37d16e2201aabc613a4c03359c7a13
Larissa Perinoto <perinotolarissa@gmail.com>
2022-11-14 10:20:19 -0300

    Insere testes de unidade da camada model.

---
+++ src/tests/unit/models/person.model.test.js
@@ -0,0 +1,47 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const connection = require('../../../models/db/connection');
+const personModel = require('../../../models/person.model');
+const { personDataBase, person, id } = require('../mock/person.mock');
+
+describe('Testa a camada model para a rota "/person"', function () {
+
+  afterEach(function () { sinon.restore() });
+
+  describe('Testa a camada model para a função "getAll"', function () {
+    it('Busca por todas as pessoas cadastradas', async function () {
+      sinon.stub(connection, 'execute').resolves([personDataBase]);
+
+      const response = await personModel.getAll();
+      expect(response).to.be.deep.equal(personDataBase);
+    });
+  });
+
+  describe('Testa a camada model para a função "insertPerson"', function () {
+    it('Faz a inserção de uma nova pessoa', async function () {
+      sinon.stub(connection, 'execute').resolves();
+
+      const response = await personModel.insertPerson(person);
+      expect(response).to.be.equal(undefined);
+    });
+  });
+
+  describe('Testa a camada model para a função "updateById"', function () {
+    it('Faz a atualização de uma nova pessoa pelo id', async function () {
+      sinon.stub(connection, 'execute').resolves();
+
+      const response = await personModel.updateById(id, person);
+      expect(response).to.be.equal(undefined);
+    });
+  });
+
+  describe('Testa a camada model para a função "getById"', function () {
+    it('Faz a busca de uma pessoa pelo id', async function () {
+      sinon.stub(connection, 'execute').resolves([person]);
+
+      const response = await personModel.getById(id);
+      expect(response).to.be.equal(person);
+    });
+  });
+});
